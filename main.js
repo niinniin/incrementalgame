b3c873be64d22e968b5bb7fe9c7e88bc438424ea
niinniin <nina.juda@gmail.com>
2016-09-27 15:28:06 +0200

    Ny bakgrund och enkel js

---
+++ main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by NinaYoda on 2016-09-27.
+ */
+
+var sushis = 0;
+
+function sushiClicker(number){
+    sushis = sushis + number;
+    document.getElementById("sushis").innerHTML = sushis;
+};
